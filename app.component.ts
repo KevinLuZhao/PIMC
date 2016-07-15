d5c0989a08856cbd1a7760b48b20028dab819683
KevinLuZhao <kevin.luzhao@gmail.com>
2016-07-15 16:52:35 -0400

    Add Nodes
    Add Nodes modules and functions

---
+++ app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from 'angular2/core';
+import {NoteComponent} from './notes/note.component';
+
+@Component({
+    selector: 'pm-app',
+    template: '<p>Hello {{title}}</p> <pm-note></pm-note>',
+    directives: [NoteComponent]
+})
+
+export /**
+ * Component
+ */
+class MyComponent {
+    title: string = "Kevin"
+}
+
+
+
---
+++ main.ts
@@ -0,0 +1,7 @@
+import { bootstrap } from 'angular2/platform/browser';
+
+// Our main component
+import { MyComponent } from './app.component';
+//import { AppComponent } from 'notes/note.component';
+
+bootstrap(MyComponent);
